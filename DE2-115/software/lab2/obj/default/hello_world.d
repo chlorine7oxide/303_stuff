2c52909f8424e15a7345ff6e8e270229ffc200cf
annabin07 <jkim960@aucklanduni.ac.nz>
2025-08-13 11:36:01 +1200

    lab2 again

---
+++ DE2-115/software/lab2/obj/default/hello_world.d
@@ -0,0 +1,26 @@
+obj/default/hello_world.o: hello_world.c ../lab2_bsp/system.h \
+ ../lab2_bsp/linker.h ../lab2_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../lab2_bsp//HAL/inc/io.h ../lab2_bsp//HAL/inc/alt_types.h sccharts.h \
+ ../lab2_bsp//HAL/inc/sys/alt_alarm.h \
+ ../lab2_bsp//HAL/inc/sys/alt_llist.h ../lab2_bsp//HAL/inc/alt_types.h \
+ ../lab2_bsp//HAL/inc/priv/alt_alarm.h
+
+../lab2_bsp/system.h:
+
+../lab2_bsp/linker.h:
+
+../lab2_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../lab2_bsp//HAL/inc/io.h:
+
+../lab2_bsp//HAL/inc/alt_types.h:
+
+sccharts.h:
+
+../lab2_bsp//HAL/inc/sys/alt_alarm.h:
+
+../lab2_bsp//HAL/inc/sys/alt_llist.h:
+
+../lab2_bsp//HAL/inc/alt_types.h:
+
+../lab2_bsp//HAL/inc/priv/alt_alarm.h:
