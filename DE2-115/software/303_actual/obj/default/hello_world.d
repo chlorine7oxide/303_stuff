ee671c18525015e3a4c183c9767058dfd1c0a1e6
annabin07 <joannakim030707@gmail.com>
2025-09-30 14:06:40 +1300

    renamed helloworld to main

--- DE2-115/software/303_actual/obj/default/hello_world.d
+++ DE2-115/software/303_actual/obj/default/hello_world.d
@@ -2,7 +2,7 @@ obj/default/hello_world.o: hello_world.c ../303_actual_bsp/system.h \
  ../303_actual_bsp/linker.h timing.h \
  ../303_actual_bsp//drivers/inc/altera_avalon_pio_regs.h \
  ../303_actual_bsp//HAL/inc/io.h ../303_actual_bsp//HAL/inc/alt_types.h \
- ../303_actual_bsp//HAL/inc/sys/alt_alarm.h \
+ scchart_heart.h ../303_actual_bsp//HAL/inc/sys/alt_alarm.h \
  ../303_actual_bsp//HAL/inc/sys/alt_llist.h \
  ../303_actual_bsp//HAL/inc/alt_types.h \
  ../303_actual_bsp//HAL/inc/priv/alt_alarm.h c_heart.h
@@ -18,6 +18,8 @@ timing.h:
 ../303_actual_bsp//HAL/inc/io.h:
 
 ../303_actual_bsp//HAL/inc/alt_types.h:
+
+scchart_heart.h:
 
 ../303_actual_bsp//HAL/inc/sys/alt_alarm.h:
 
