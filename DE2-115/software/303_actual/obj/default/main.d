3618a3f049ae27ef9ec82018d425b6af364d5713
annabin07 <joannakim030707@gmail.com>
2025-09-30 15:09:34 +1300

    no progress

---
+++ DE2-115/software/303_actual/obj/default/main.d
@@ -0,0 +1,32 @@
+obj/default/main.o: main.c ../303_actual_bsp/system.h \
+ ../303_actual_bsp/linker.h timing.h \
+ ../303_actual_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../303_actual_bsp//HAL/inc/io.h ../303_actual_bsp//HAL/inc/alt_types.h \
+ scchart_heart.h ../303_actual_bsp//HAL/inc/sys/alt_alarm.h \
+ ../303_actual_bsp//HAL/inc/sys/alt_llist.h \
+ ../303_actual_bsp//HAL/inc/alt_types.h \
+ ../303_actual_bsp//HAL/inc/priv/alt_alarm.h c_heart.h
+
+../303_actual_bsp/system.h:
+
+../303_actual_bsp/linker.h:
+
+timing.h:
+
+../303_actual_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../303_actual_bsp//HAL/inc/io.h:
+
+../303_actual_bsp//HAL/inc/alt_types.h:
+
+scchart_heart.h:
+
+../303_actual_bsp//HAL/inc/sys/alt_alarm.h:
+
+../303_actual_bsp//HAL/inc/sys/alt_llist.h:
+
+../303_actual_bsp//HAL/inc/alt_types.h:
+
+../303_actual_bsp//HAL/inc/priv/alt_alarm.h:
+
+c_heart.h:
